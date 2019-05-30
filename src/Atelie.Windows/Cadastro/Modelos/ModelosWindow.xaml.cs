6a5438ab5d222510abea243c588c9302aa1f9df2
Marcelo Dias <mardsystems@gmail.com>
2019-05-30 20:55:30 -0300

    Ajustes.

--- src/Atelie.Windows/Cadastro/Modelos/ModelosWindow.xaml.cs
+++ src/Atelie.Windows/Cadastro/Modelos/ModelosWindow.xaml.cs
@@ -85,20 +85,20 @@ namespace Atelie.Cadastro.Modelos
         }
     }
 
-    public class ModeloValidationRule : ValidationRule
-    {
-        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
-        {
-            ModeloViewModel viewModel = (value as BindingGroup).Items[0] as ModeloViewModel;
+    //public class ModeloValidationRule : ValidationRule
+    //{
+    //    public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+    //    {
+    //        ModeloViewModel viewModel = (value as BindingGroup).Items[0] as ModeloViewModel;
 
-            if (viewModel.HasErrors)
-            {
-                return new ValidationResult(false, viewModel.Error);
-            }
-            else
-            {
-                return ValidationResult.ValidResult;
-            }
-        }
-    }
+    //        if (viewModel.HasErrors)
+    //        {
+    //            return new ValidationResult(false, viewModel.Error);
+    //        }
+    //        else
+    //        {
+    //            return ValidationResult.ValidResult;
+    //        }
+    //    }
+    //}
 }
