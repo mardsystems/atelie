ed67e8bf98526de2f0fa40d8e7ae97f7b155eb0c
Marcelo Dias <mardsystems@gmail.com>
2019-06-04 00:09:01 -0300

    Styles.

--- src/Atelie.Windows/App.xaml.cs
+++ src/Atelie.Windows/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,15 @@ namespace Atelie
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var culture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+            Thread.CurrentThread.CurrentCulture = culture;
+
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
     }
 }
--- src/Atelie.Windows/Properties/AssemblyInfo.cs
+++ src/Atelie.Windows/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Atelie.Windows")]
+[assembly: AssemblyTitle("Atelie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Atelie.Windows")]
+[assembly: AssemblyProduct("Atelie")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
+
